89e873d4a62481dc3f22761138fd479a52e6b852
Sajid <sajid.abbas.mme@gmail.com>
2022-11-30 14:37:20 +0500

    components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="whole">
+      <div className="left">
+        <h1 className="heading1">Team Details</h1>
+        <h3 className="heading3">Team Name</h3>
+        <input type="text" className="searchbar" />
+        <h1 className="access">Workflow access</h1>
+        <input type="text" className="workaccess" />
+        <div>
+          <button className="btn-primary">
+            <span className='Boss'>Analytics</span>
+          </button>
+          <button className="btn-primary1">
+            <span className='Boss'>Accounts</span>
+            </button>
+          <button className="btn-primary2"><span className='Boss'>Shipping</span>
+          </button>
+          <button className="btn-primary3"><span className='Boss'>Inventory</span>
+          </button>
+        </div>
+        <h3 className="headings">Invite Message</h3>
+        <div className="texta">
+          <div className="textb">You have been added to this team and have access to new workflows. Check your Fueled.ai profile!</div>
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ src/components/Dashboard4_1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard4_1() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
